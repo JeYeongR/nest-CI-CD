95d03444a51b4de2bd290246fca91875496c6a29
JeYeongR <wpdud2003@gmail.com>
2024-01-25 04:20:24 +0900

    Feat: deploy 구현

---
+++ scripts/after-deploy.sh
@@ -0,0 +1,16 @@
+echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" >> /home/ec2-user/deploy.log
+CURRENT_PID=$(sudo docker container ls -q)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ubuntu/deploy.log
+else
+  echo "> sudo docker stop $CURRENT_PID"   # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
+  sudo docker stop $CURRENT_PID
+  sleep 5
+fi
+
+cd /home/ubuntu/build
+sudo docker pull wpdud2003/nest-ci-cd-test
+sudo docker run -d -p 80:80 wpdud2003/nest-ci-cd-test
+sudo pm2 start dist/main.js
